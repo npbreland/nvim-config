a65077c80e8232c2ab5aa433454dcabe7cef424e
Nick Breland <npbreland@tutanota.com>
2022-11-23 11:55:21 +0000

    Print PHP CS Fixer output to messages

--- lua/nick/set.lua
+++ lua/nick/set.lua
@@ -24,8 +24,9 @@ vim.api.nvim_create_autocmd({"BufWriteCmd"}, {
     pattern = {"*.php"},
     callback = function() 
         local bufname = vim.api.nvim_buf_get_name(0)
-        local cmd_str = "php-cs-fixer fix %s --rules=@PSR12 --quiet"
-        os.execute(string.format(cmd_str, bufname))
+        local cmd_str = "php-cs-fixer fix %s --rules=@PSR12 -v"
+        cmd_str = string.format(cmd_str, bufname)
+        print(vim.fn.system(cmd_str))
         vim.cmd(":e")
     end
 })
