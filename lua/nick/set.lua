39775bb7ee9efbdc6455e0856b6aeed1ee2bdfc4
Nick Breland <npbreland@tutanota.com>
2022-11-23 11:31:42 +0000

    Add PSR-12 fixing

--- lua/nick/set.lua
+++ lua/nick/set.lua
@@ -18,3 +18,15 @@ vim.opt.colorcolumn = "80"
 vim.api.nvim_set_option("clipboard","unnamed")
 
 vim.g.mapleader = " "
+
+-- Apply PSR-12 on save to all PHP files
+vim.api.nvim_create_autocmd({"BufWriteCmd"}, {
+    pattern = {"*.php"},
+    callback = function() 
+        local bufname = vim.api.nvim_buf_get_name(0)
+        local cmd_str = "php-cs-fixer fix %s --rules=@PSR12 --quiet"
+        os.execute(string.format(cmd_str, bufname))
+        vim.cmd(":e")
+    end
+})
+
