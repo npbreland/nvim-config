647482eee7566f0694a0d88cbc1beb50cb32e588
Nick Breland <npbreland@tutanota.com>
2022-12-13 12:40:44 -0600

    Run php-cs-fixer quietly

--- lua/nick/format.lua
+++ lua/nick/format.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd({"BufWritePost"}, {
     pattern = {"*.php"},
     callback = function() 
         local bufname = vim.api.nvim_buf_get_name(0)
-        local cmd_str = "php-cs-fixer fix %s --rules=@PSR12 -v"
+        local cmd_str = "php-cs-fixer fix %s --rules=@PSR12 --quiet"
         cmd_str = string.format(cmd_str, bufname)
         print(vim.fn.system(cmd_str))
         vim.cmd('edit') -- reload
